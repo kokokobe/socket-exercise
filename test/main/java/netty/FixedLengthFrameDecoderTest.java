1f4b97bae532cca0aa1fb06cf0b7210a072071bc
BriLiang <928570005@qq.com>
2014-06-23 21:59:54 +0800

    unit test using EmbeddedChannel

--- test/main/java/netty/FixedLengthFrameDecoderTest.java
+++ test/main/java/netty/FixedLengthFrameDecoderTest.java
@@ -30,4 +30,23 @@ public class FixedLengthFrameDecoderTest {
         Assert.assertEquals(buf.readBytes(3),channel.readInbound());
         Assert.assertNull(channel.readInbound());
     }
+    @Test
+    public void testFramesDecoded2(){
+        ByteBuf buf= Unpooled.buffer();
+        for (int i=0;i<9;i++){
+            buf.writeByte(i);
+        }
+        ByteBuf input=buf.duplicate();
+        EmbeddedChannel channel=new EmbeddedChannel(new FixedLengthFrameDecoder(3));
+        Assert.assertFalse(channel.writeInbound(input.readBytes(2)));
+        Assert.assertTrue(channel.writeInbound(input.readBytes(7)));
+        /**
+         * write finish
+         */
+        Assert.assertTrue(channel.finish());
+        Assert.assertEquals(buf.readBytes(3), channel.readInbound());
+        Assert.assertEquals(buf.readBytes(3), channel.readInbound());
+        Assert.assertEquals(buf.readBytes(3), channel.readInbound());
+        Assert.assertNull(channel.readInbound());
+    }
 }
