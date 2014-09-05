961eeb5b02f2fcf56b52e6b3eb231aaa4ed6cca6
liangwen.liang <liangwen.liang@vipshop.com>
2014-09-05 18:13:35 +0800

    add zookeeper test

---
+++ test/main/java/zookeeper/zookeeperTest1.java
@@ -0,0 +1,56 @@
+package zookeeper;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author BriLiang(liangwen.liang@vipshop.com)
+ * @date 2014/9/5.
+ * Description:(zookeeper client test
+ * client port :2181,2182,2183)
+ */
+public class zookeeperTest1 {
+    private static Integer server1ClientPort = 2181;
+    private static Integer server2ClientPort = 2182;
+    private static Integer server3ClientPort = 2183;
+
+    @Test
+    public void test1() {
+        // 创建一个与服务器的连接
+        try {
+
+            ZooKeeper zk = new ZooKeeper("localhost:" + server1ClientPort, 3000, event -> System.out.println("已经触发了" + event.getType() + "事件！"));
+            /*创建一个目录节点*/
+            String path=zk.create("/testRootPath","testRootData".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("path:"+path);
+            System.out.println(new String(zk.getData("/testRootPath",false,null)));
+            // 创建一个子目录节点
+            zk.create("/testRootPath/testChildPathOne", "testChildDataOne".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+            // 取出子目录节点列表
+            System.out.println("childOne:"+zk.getChildren("/testRootPath",true));
+            // 修改子目录节点数据
+            zk.setData("/testRootPath/testChildPathOne","modifyChildDataOne".getBytes(),-1);
+            System.out.println("目录节点状态：["+zk.exists("/testRootPath",true)+"]");
+            // 创建另外一个子目录节点
+            zk.create("/testRootPath/testChildPathTwo", "testChildDataTwo".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+            System.out.println("childTwo:"+new String(zk.getData("/testRootPath/testChildPathTwo",true,null)));
+            /*删除子目录节点*/
+            zk.delete("/testRootPath/testChildPathOne",-1);
+            zk.delete("/testRootPath/testChildPathTwo",-1);
+            /*删除父节点*/
+            zk.delete("/testRootPath",-1);
+            zk.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+}
