37a4ff41ec334e3cd2dbd360da34b8a51acd4027
liangwen.liang <liangwen.liang@vipshop.com>
2014-10-10 18:09:55 +0800

    add zookeeper watcher for app1

---
+++ src/main/java/com/liang/zookeeper/watcher/WatcherClient.java
@@ -0,0 +1,65 @@
+package com.liang.zookeeper.watcher;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author BriLiang(liangwen.liang@vipshop.com)
+ * @date 2014/10/10.
+ * Description:()
+ */
+public class WatcherClient implements Runnable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WatcherClient.class);
+    public static final int CLIENT_PORT = 6150;
+    /*所要监控的结点*/
+    public static final String PATH = "/app1";
+    /*所要监控的结点的子结点列表*/
+    protected static List<String> nodeList;
+    protected static ZooKeeper zk;
+    protected WatcherForNode watcherForNode = new WatcherForNode();
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                zk.exists(PATH, watcherForNode);
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                nodeList = zk.getChildren(PATH, watcherForNode);
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            /*对PATH下的每个结点都设置一个watcher*/
+            for (String nodeName : nodeList) {
+                try {
+                    zk.exists(PATH + "/" + nodeName, watcherForNode);
+                } catch (KeeperException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public WatcherClient() throws IOException {
+        zk = new ZooKeeper("localhost:" + CLIENT_PORT, 21810, watcherForNode);
+    }
+
+    public static void main(String[] args) throws IOException {
+        WatcherClient client = new WatcherClient();
+        Thread th = new Thread(client);
+        th.start();
+    }
+}
---
+++ src/main/java/com/liang/zookeeper/watcher/WatcherForNode.java
@@ -0,0 +1,48 @@
+package com.liang.zookeeper.watcher;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author BriLiang(liangwen.liang@vipshop.com)
+ * @date 2014/10/10.
+ * Description:()
+ */
+public class WatcherForNode implements Watcher {
+    private static final Logger log= LoggerFactory.getLogger(WatcherForNode.class);
+    @Override
+    public void process(WatchedEvent event) {
+        /*结点变化之前的结点列表*/
+        List<String> nodeListBefore=WatcherClient.nodeList;
+        /*主结点的数据变化时*/
+        if(event.getType()== Event.EventType.NodeDataChanged){
+            log.info("Node data changed:" + event.getPath());
+        }
+        if (event.getType() == Event.EventType.NodeDeleted){
+            log.info("Node deleted:" + event.getPath());
+        }
+        if(event.getType()== Event.EventType.NodeCreated){
+            log.info("Node created:"+event.getPath());
+        }
+        /*获取更新后的NodeList*/
+        List<String> nodeListNow = null;
+        try {
+            nodeListNow=WatcherClient.zk.getChildren(event.getPath(),false);
+        } catch (KeeperException e) {
+            log.error("zookeeper has an error for",e);
+        } catch (InterruptedException e) {
+            log.error("zookeeper interrupted",e);
+        }
+        /*增加新节点输出新节点的日志*/
+        if(nodeListBefore.size()<nodeListNow.size()){
+            nodeListNow.stream().filter(str -> !nodeListBefore.contains(str)).forEach(str -> {
+                log.info("Node created:" + event.getPath() + "/" + str);
+            });
+        }
+    }
+}
