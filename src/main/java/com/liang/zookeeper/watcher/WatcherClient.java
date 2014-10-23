8d386da81056fd46fc7b0bffc3eae9f5e4906e60
liangwen.liang <liangwen.liang@vipshop.com>
2014-10-23 10:56:33 +0800

    add apache zookeeper framework

--- src/main/java/com/liang/zookeeper/watcher/WatcherClient.java
+++ src/main/java/com/liang/zookeeper/watcher/WatcherClient.java
@@ -4,7 +4,6 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
