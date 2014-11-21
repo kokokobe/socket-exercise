ec651f4aa64ea9d92fdbde25ff47e207e26bcdf0
liangwen.liang <liangwen.liang@vipshop.com>
2014-11-21 18:36:56 +0800

    add UDP example for netty

--- src/main/java/com/liang/netty/udp/LogEvent.java
+++ src/main/java/com/liang/netty/udp/LogEvent.java
@@ -15,20 +15,21 @@ import java.net.InetSocketAddress;
  * Description:()
  */
 public class LogEvent {
-    public static final byte SEPARATOR = (byte)':';
+    public static final byte SEPARATOR = (byte) ':';
     private final InetSocketAddress source;
     private final String logFile;
     private final String msg;
     private final long received;
 
-    public LogEvent(InetSocketAddress source, String logFile, String msg, long received) {
+    public LogEvent(InetSocketAddress source, long received, String logFile, String msg) {
         this.source = source;
         this.logFile = logFile;
         this.msg = msg;
         this.received = received;
     }
-    public LogEvent(String logFile,String msg){
-        this(null,logFile,msg,-1);
+
+    public LogEvent(String logFile, String msg) {
+        this(null, -1, logFile, msg);
     }
 
     public InetSocketAddress getSource() {
---
+++ src/main/java/com/liang/netty/udp/LogEventBroadcaster.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) BriLiang All rights reserved.
+ * File create : 14-11-21 下午4:48
+ * Project Name: socket-exercise
+ * File Name: LogEventBroadcaster.java
+ */
+
+package com.liang.netty.udp;
+
+import com.liang.netty.encoder.LogEventEncoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author BriLiang(liangwen.liang@vipshop.com)
+ * @date 2014/11/21.
+ * Description:(config broadcaster channelPipeLine)
+ */
+public class LogEventBroadcaster {
+    private final EventLoopGroup eventLoopGroup;
+    private final Bootstrap bootstrap;
+    private final File file;
+
+    public LogEventBroadcaster(InetSocketAddress socketAddress, File file) {
+        this.eventLoopGroup = new NioEventLoopGroup();
+        this.bootstrap = new Bootstrap().group(eventLoopGroup).channel(NioDatagramChannel.class)
+                .option(ChannelOption.SO_BROADCAST, true).handler(new LogEventEncoder(socketAddress));
+        this.file = file;
+    }
+
+    public void run() throws IOException {
+        Channel channel = bootstrap.bind(0).syncUninterruptibly().channel();
+        long pointer = 0;
+        for (; ; ) {
+            long len = file.length();
+            if (len <= pointer) {
+                /*file was reset*/
+                pointer = len;
+            } else if (len > pointer) {
+                /*Content was added*/
+                /* read only*/
+                RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+                randomAccessFile.seek(pointer);
+                String line;
+                while ((line = randomAccessFile.readLine()) != null) {
+                    channel.write(new LogEvent(null, -1, file.getAbsolutePath(), line));
+                }
+                pointer = randomAccessFile.getFilePointer();
+                randomAccessFile.close();
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                Thread.interrupted();
+                break;
+            }
+        }
+    }
+
+    public void shutdown() {
+        eventLoopGroup.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\lwl\\.IntelliJIdea14\\system\\tomcat\\Unnamed_vips-mobile-operation\\logs\\catalina.2014-11-11.log";
+        LogEventBroadcaster broadcaster = new LogEventBroadcaster(new InetSocketAddress("255.255.255.255", 1036), new File(filePath));
+        try {
+            broadcaster.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            broadcaster.shutdown();
+        }
+
+    }
+}
